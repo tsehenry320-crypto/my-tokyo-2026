66605c895721aa5dd731e2cfcc10c366ddeaef94
tsehenry320-crypto <tsehenry320@gmail.com>
2026-04-29 00:32:50 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-//import './index.css' // 如果你沒這個檔案，先註解掉這行也行
+import App from './App' // 確保這裡寫的是 './App' 而不是 '/App' 或 './src/App'
+
+// 如果你沒有 index.css 檔案，一定要把這行刪掉
+// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
